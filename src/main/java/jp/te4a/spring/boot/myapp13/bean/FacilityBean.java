4483ff88f99d01bbb9832d160e6d44237bc94792
Shoki <syoki0415@gmail.com>
2025-07-08 11:08:37 +0900

    Co-authored-by: Tsubasa-Abe <Tsubasa-Abe@users.noreply.github.com>

--- src/main/java/jp/te4a/spring/boot/myapp13/bean/FacilityBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/bean/FacilityBean.java
@@ -23,4 +23,18 @@ public class FacilityBean {
     private String writter;
     private String publisher;
     private Integer price;
+
+    /*private Integer no;
+
+    private String pro_name; //品名
+    private String model_no; //型番
+    private String maker; //メーカ
+    private String siyou; //仕様
+    private Integer t_year; //耐用年数
+    private Integer date; //購入日
+    private String genka_syoukyaku; //減価償却
+    private boolean useable; //使用可能
+    private boolean lentable; //貸出可能
+    private String place; //設置場所
+    private Integer limit; //使用期限*/
 }
--- src/main/java/jp/te4a/spring/boot/myapp13/form/FacilityForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/form/FacilityForm.java
@@ -14,13 +14,27 @@ import lombok.NoArgsConstructor;
 public class FacilityForm {
     private Integer id;
     @NotNull
-    @Size(min= 3)
-    @TestValid(param = "abc")
+    //@Size(min= 3)
+    //@TestValid(param = "abc")
     private String title;
     @Size(min= 3,max = 20)
-    @Writter(ok = "東北タロウ")
+    //@Writter(ok = "東北タロウ")
     private String writter;
     private String publisher;
-    @Min(0)
+    //@Min(0)
     private Integer price;
+
+    private Integer no;
+
+    private String pro_name; //品名
+    private String model_no; //型番
+    private String maker; //メーカ
+    private String siyou; //仕様
+    private Integer t_year; //耐用年数
+    private Integer date; //購入日
+    private String genka_syoukyaku; //減価償却
+    private boolean useable; //使用可能
+    private boolean lentable; //貸出可能
+    private String place; //設置場所
+    private Integer limit; //使用期限
 }
